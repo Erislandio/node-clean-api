1ec3494f7708397a4bea47c3a122f5e3cd11c053
erislandio.soares <erislandiosoares21@gmail.com>
2020-10-30 15:34:37 -0300

    test: ensure SingUpCOntroller return an error if no email is porvided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -6,15 +6,31 @@ describe('Signup Controller', () => {
 
     const httpRequest = {
       body: {
-        name: 'Erislandio',
-        email: 'any@email.com',
-        password: 'Er1sl@ndio',
-        passwordConfirmation: 'Er1sl@ndio'
+        name: 'any_name',
+        email: 'any@email',
+        password: 'any_pass',
+        passwordConfirmation: 'any_pass'
       }
     }
 
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SingUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_pass',
+        passwordConfirmation: 'Er1sl@ndio'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
