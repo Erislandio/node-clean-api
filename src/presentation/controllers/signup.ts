a5912e879617b61d6260dbffa719623d857ba71e
erislandio.soares <erislandiosoares21@gmail.com>
2020-10-30 16:06:19 -0300

    feat: add interface Controller on SignUpController

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,10 @@
 import { MissingParamError } from './errors/missingParamError'
 import { badRequest } from './helpers/httpHelper'
-import { httpRequest, HttpResponse } from './protocols/http.interface'
+import { Controller } from './protocols/controller.interface'
+import { HttpRequest, HttpResponse } from './protocols/http.interface'
 
-export class SingUpController {
-  public handle (httpRequest: httpRequest): HttpResponse {
+export class SingUpController implements Controller {
+  public handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
