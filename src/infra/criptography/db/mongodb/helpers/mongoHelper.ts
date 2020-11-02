0aa8c26fa9d371c36e4217c6422de749ae1c10ec
erislandio.soares <erislandiosoares21@gmail.com>
2020-11-02 15:29:44 -0300

    test: ensure AccountMongoRepository returns an account on success

--- src/infra/criptography/db/mongodb/helpers/mongoHelper.ts
+++ src/infra/criptography/db/mongodb/helpers/mongoHelper.ts
@@ -1,7 +1,7 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
-  client: null as MongoClient,
+  client: MongoClient,
   async connect (uri: string): Promise<void> {
     this.client = await MongoClient.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
@@ -11,6 +11,10 @@ export const MongoHelper = {
 
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 
 }
