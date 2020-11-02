4216f2ff011e7e6885620aa0a984f3a489ead341
erislandio.soares <erislandiosoares21@gmail.com>
2020-11-02 12:00:03 -0300

    test: move bycript hash method to a mock function

--- src/infra/criptography/bycriptAdpter.spec.ts
+++ src/infra/criptography/bycriptAdpter.spec.ts
@@ -1,16 +1,28 @@
 import { BcryptAdpter } from './bycriptAdpter'
 import bcrypt from 'bcrypt'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return new Promise(resolve => resolve('hash'))
+  }
+}))
+
 const makeSut = (): BcryptAdpter => {
   const salt = 12
   return new BcryptAdpter(salt)
 }
 
 describe('BcryotAdpter', () => {
-  test('Shoul call bycript with corret value', async () => {
+  test('Should call bycript with corret value', async () => {
     const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', 12)
+  })
+
+  test('Should return a hash on success', async () => {
+    const sut = makeSut()
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hash')
   })
 })
