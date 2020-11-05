cc9326d945280b6c42334aa3fb0ceacabec1d8aa
erislandio.soares <erislandiosoares21@gmail.com>
2020-11-05 19:17:47 -0300

    feat: add LogControllerDecorator in SignUpController

--- src/main/config/factories/signup.ts
+++ src/main/config/factories/signup.ts
@@ -3,13 +3,15 @@ import { SingUpController } from '../../../presentation/controllers/signup/signu
 import { EmailValidatorAdapter } from '../../../utils/emailValidatorAdpter'
 import { BcryptAdpter } from '../../../infra/criptography/bycriptAdapter'
 import { AccountMongoRepository } from '../../../infra/criptography/db/mongodb/accountRepository/account'
+import { Controller } from '../../../presentation/protocols'
+import { LogControllerDecorator } from '../../decorators/log'
 
-export const makeSignupController = (): SingUpController => {
+export const makeSignupController = (): Controller => {
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const bcryptAdpter = new BcryptAdpter()
   const addAccountRepo = new AccountMongoRepository()
   const addAccount = new DbAddAccount(bcryptAdpter, addAccountRepo)
 
   const singUpController = new SingUpController(emailValidatorAdapter, addAccount)
-  return singUpController
+  return new LogControllerDecorator(singUpController)
 }
