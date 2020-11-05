0a91aa67d7ed236f7f7acef335afd14200664469
erislandio.soares <erislandiosoares21@gmail.com>
2020-11-05 19:18:35 -0300

    test: add LogControllerDecorator in SignUpController

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,49 @@
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+import { LogControllerDecorator } from './log'
+
+const makeControllerStub = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          id: 'valid_id',
+          email: 'any@email.com',
+          name: 'any_name',
+          password: 'any_pass',
+          passwordConfirmation: 'any_pass'
+        }
+      }
+
+      return new Promise(resolve => resolve(httpResponse))
+    }
+  }
+
+  return new ControllerStub()
+}
+
+const makeSut = (stub: Controller): Controller => {
+  const sut = new LogControllerDecorator(stub)
+
+  return sut
+}
+
+describe('Log controller decorator', () => {
+  const controllerStub = makeControllerStub()
+  const sut = makeSut(controllerStub)
+  test('Should call controller handle', async () => {
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+
+    const httpRequest = {
+      body: {
+        email: 'any@email.com',
+        name: 'any_name',
+        password: 'any_pass',
+        passwordConfirmation: 'any_pass'
+      }
+    }
+
+    await sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+})
